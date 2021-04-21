bbb8d5de0fb356034fd21dd6b82094545e9be72d
2018030489 <2018030489@upsin.edu.mx>
2021-04-20 21:12:13 -0600

    initial commit

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const app = require('./app');
 const sequelize = require('./db');
 
-const port = 3007; //puerto asignado
+const port = 3010;
 
 (async ()=>{
     await sequelize.sync({ force: false, alter: true});
     app.listen(port,
-        ()=> console.info(`Server running on port ${port} http://201.140.116.237:${port}`),//direccion ip
-        //direccion 201.140.116.237
+        ()=> console.info(`Server running on port ${port} http://201.140.116.237${port}`),
     );
 })();
 
 app.get('/',function(req,res){
-    res.render('./formulario.ejs')
+    res.render('formulario.ejs')
 })
